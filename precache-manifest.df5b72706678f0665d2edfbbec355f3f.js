6d7a6b1d82c0c58e3f8f2f772010d097fa20d0a9
rishabhrai09 <rishabh8526@gmail.com>
2020-05-20 15:33:54 +0530

    Updates

---
+++ precache-manifest.df5b72706678f0665d2edfbbec355f3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848a678f5a250b780087507db622890e",
+    "url": "/moviesApp/index.html"
+  },
+  {
+    "revision": "b9632d24748a43ef53af",
+    "url": "/moviesApp/static/css/main.07d3050c.chunk.css"
+  },
+  {
+    "revision": "0bae73921afea83da83e",
+    "url": "/moviesApp/static/js/2.46519c71.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/moviesApp/static/js/2.46519c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9632d24748a43ef53af",
+    "url": "/moviesApp/static/js/main.5c393720.chunk.js"
+  },
+  {
+    "revision": "2acf3b99aa3c51147236",
+    "url": "/moviesApp/static/js/runtime-main.32d2b6cb.js"
+  }
+]);
